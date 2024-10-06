40fae5900c6abd5d654f8d5ceb2211fd4c85dbd3
Renata Mendes <52473030+reehmendes@users.noreply.github.com>
2024-10-06 19:11:40 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Reprodução do tabnews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 //criando uma rota publica no caso da página principal
-function Home(){
-    return <h1>Espero que sorria no dia de hoje!</h1>
+function Home() {
+  return <h1>Espero que sorria no dia de hoje!</h1>;
 }
 
-export default Home; //a partir dele um componente chama o outro 
+export default Home; //a partir dele um componente chama o outro
